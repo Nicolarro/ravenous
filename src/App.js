ba11449fa6000887a1af71aa39cd5449612692a9
Nicolas Larrosa <nicolas.larrosa@accenture.com>
2023-04-21 12:11:05 -0300

    creating proyect

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Ravenous">
+        <h1>
+            Ravenous Proyect
+        </h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
